1efdf69e9966365705e682104eee0d717a5ff12b
jakka351 <57064943+jakka351@users.noreply.github.com>
2021-11-28 23:46:48 +1100

    Update can0spoti.py

--- can0spoti.py
+++ can0spoti.py
@@ -3,7 +3,7 @@
 # https://github.com/jakka351/FG-Falcon | https://github.com/jakka351/can0swc
 # assumes ms-can is up on can0
 # buffers several CAN IDS, then matches byte specific data
-# when it matches, emits keypress
+# this version rigged up to work with raspotify - control spotify connect via canbus
 ############################
 # Import modules
 ############################
@@ -27,36 +27,38 @@ SWC_PHONE              = (0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0xC1, 
 SWC_MODE               = (0x10)
 AuxillaryMode          = (0x41, 0xC1)
 RadioPhoneMode         = (0x43, 0x44, 0x45, 0x47, 0x48, 0xC3, 0xC4, 0xC5, 0xC7, 0xC8)
+modeswap = 0
+counter = 0
 def scroll():
     #prints logo to console
         print('  ')
         print('''  
              ,777I77II??~++++=~::,,,,::::::::::~~~==+~                                        
-           ,IIIIII,IIII+~..,,:,,,,,:~:,,.....,~,,:::~+?+?=                                    
+           ,IIIIII,IIII+~R,:A, S,,P:~O:,T..I.,F,,Y:::~+?+?=                                    
          :=I7777I=IIIIII~...:===,:,=~:,,,,,,::=,,,:::~~:=?===                                 
       ~=,?I?777II7IIIII=~,.,,,,,,,,,,,:,,,,,,::,,,,~:~~~~:~+:~:~                              
       I=I?IIIIII~IIIIIII+:..,,,,,,,,,,,,.,.,,::.,,,,:::~~=~:=+~?~~                            
       I77?+?IIIIIIIII7I7=~,.,,,..,,,,.,.,.......,.,.,.,..,,,:~=~:==~~                         
      +=I7777I?+???IIIIII+=:..,,,,,,,,,,,...,,,,,,,,,,,,..,,,:..:?I7+...,,                     
-     +=+=I7777I=~~+~:~IIII~,..,,,,,,,,,,..,,,,,...~+II?I?+?III7IIII777I7=.....                
-      ==++++III=~~~::~+I:+?~:.........:+IIIIIIII+=?IIIIIII???????????III7II7I....             
+     +=+=I7777I=~~+~:~IIII~,..,,,,,,,,,,..,,,,,...~+II?I?+?IIHECTICII77I7=.....                
+      ==++++III=~~~::~+I:+?~:.........:+IIIIIIII+=?IIJACK????????????III7II7I....             
      ?+=  ██████  █████  ███    ██  ██████  ███████ ██     ██  ██████++++???II?III....         
-     ?+= ██      ██   ██ ████   ██ ██  ████ ██      ██     ██ ██     ======++?II?+7II.         
+     ?+= ██ C    ██ A ██ ████ N ██ ██B ████ ██ U    ██     ██ ██   S ======++?II?+7II.         
      ??+ ██      ███████ ██ ██  ██ ██ ██ ██ ███████ ██  █  ██ ██     ~~~~======+???++II.       
-     ??+ ██      ██   ██ ██  ██ ██ ████  ██      ██ ██ ███ ██ ██     ~~~~~~~~~===++++=II,      
+     ??+ ██      ██   ██ ██  ██ ██ ████  ██      ██ ██ ███ ██ ██     ~~~~~~~~~===++++=II,      
      I??  ██████ ██   ██ ██   ████  ██████  ███████  ███ ███   ██████:::~~~~~~~~~====+==I,     
-      ?I+=~~fg+falcon+swc+adapter+??++++===~:~~~~~~~=???=?:=~~~~~::::::::~~~~~~~~~~=~=+?7~:    
-       ?+=~~~~=++~~~~~+???~=?7II??+++++++==~~:~~~~~~:~~???=+:~~~~~~:::::::::~~~~=++:,.,+=,,   
-        =?I+~~~==++~~:+??~====+I?+===+++++==~~~::::::::::~????~~~~~:::::~:~==+:,,,,?..::+=:   
-          =?I=~~~==++=+++~==:,~~=+====+++++:,,...:,::~:::::~~~~+~:~~:~~==,.,,.?I~,..::~~=,:   
-           ~=+I?=~~=~+++~~=...,~:=+====++?:~+=?I~,I=I??..~III7I:==~,.,,.,,.,,,...::~~++~:~:   
-              ~?I+=~~~~+~~~.:+,,,:=+===+++~+==~=+:III=?I?77777I~~~===,,,,.,.,,~~~~=+=~::,,:   
-                ~?I+=:~~~~~~,,+:,,+==~+++++,:~~:==,??,:,,=??I++,,,:~===,,,::~~=++=:::~=..,,   
-             ,,,,:=+?==~~~~.=:~~,..,=+++++=~:=+=~:.,:,,,,::?=I=:::::+~====++++=~:::?I+?,..,   
-              :,,,,~+====~:,,,:=,.,,,~~===~~~,:==~~~~~~:..,,,,,,..,,,~==+++~:,~++I++II?...    
-               :,,,,,,+==+,:..:==.:,,~:~~~~~:,,,,:~~~~~~~~=========~++++~,....II+II+?...,:    
-                 ,,,,,,,++.,,.,,,,=:,,~:::~:::,,,,,,,,,::~~~=====~====~.......?I=I.....,:~    
-                   ::,,,,,,:~::,~+I+,..~::::::,,,,,,,,,,,,,,,,,~==~~~.........+.......,:,,    ''')
+      ?I+=~~fg+falcon+swc+adapter+??++++===~:~~~~~~~=???=?:=H~~~~~::::::::~~~~~~~~~~=~=+?7~:    
+       ?+=~~~~=++~~~~~+???~=?7II??+++++++==~~:~~~~~~:~~???=+:I~~~~~~:::::::::~~~~=++:,.,+=,,   
+        =?I+~~~==++~~:+??~====+I?+===+++++==~~~::::::::::~????T~~~~~:::::~:~==+:,,,,?..::+=:   
+          =?I=~~~==++=+++~==:,~~=+====+++++:,,...:,::~:::::~~~~E~:~~:~~==,.,,.?I~,..::~~=,:   
+           ~=+I?=~~=~+++~~=...,~:=+====++?:~+=?I~,I=I??..~III7I:=F=~,.,,.,,.,,,...::~~++~:~:   
+              ~?I+=~~~~+~~~.:+,,,:=+===+++~+==~=+:III=?I?LOL77I~~~O===,,,,.,.,,~~~~=+=~::,,:   
+                ~?I+=:~~~~~~,,+:,,+==~+++++,:~~:==,??,:,,=??I++,,,: R===,,,::~~=++=:::~=..,,   
+             ,,,,:=+?==~~~~.=:~~,..,=+++++=~:=+=~:.,:,,,,::?=I=:::::+~D====++++=~:::?I+?,..,   
+              :,,,,~+====~:,,,:=,.,,,~~===~~~,:==~~~~~~:..,,,,,,..,,,~==S+++~:,~++I++II?...    
+               :,,,,,,+==+,:..:==.:,,~:~~~~~:,,,,:~~~~~~~~=========~++++~,M....II+II+?...,:    
+                 ,,,,,,,++.,,.,,,,=:,,~:::~:::,,,,,,,,,::~~~=====~====~......E.?I=I.....,:~    
+                   ::,,,,,,:~::,~+I+,..~::::::,,,,,,,,,,,,,,,,,~==~~~.........+L....L..S:,,    ''')
         
 def setup():
     global bus
@@ -67,7 +69,7 @@ def setup():
     print("                      ")
     print("        CANbus active on", bus)   
     print("        waiting for matching can frame...")     #this line gets replaced by the next matching can frame
-    print("        ready to emit keypress...")             # this line gets replaced by the button in the car that is pushed
+    print("        Raspotify Control via CAN...")             # this line gets replaced by the button in the car that is pushed
     
 def msgbuffer():
     global message, q, SWC
@@ -80,7 +82,7 @@ def msgbuffer():
         #if message.arbitration_id == ICC:                        
         #    q.put(message)
 def main(): 
-    global message, bus, q, SWC, SWC_SEEK, SWC_VOLUP, SWC_VOLDOWN, SWC_PHONE, AuxillaryMode
+    global message, bus, q, SWC, SWC_SEEK, SWC_VOLUP, SWC_VOLDOWN, SWC_PHONE, AuxillaryMode, RadioPhoneMode, counter, modeswap
     try:
         while True:
             for i in range(8):
@@ -88,13 +90,9 @@ def main():
                     pass
                 message = q.get()   
                 c = '{0:f},{1:d},'.format(message.timestamp,count)
-                if message.arbitration_id == SWC and message.data[6] in AuxillaryMode:
-                    os.system("./rpotify.sh play")
-                    os.system("./rpotify.sh status")
-                    print("AuxillaryMode Active @", message.timestamp)                    
-                    time.sleep(0.5)
-                
-                elif message.arbitration_id == SWC and message.data[7] in SWC_SEEK:
+              
+
+                if message.arbitration_id == SWC and message.data[7] in SWC_SEEK:
                     os.system("./rpotify.sh next")
                     print("SWCSeekBtn pushed @", message.timestamp)
                     time.sleep(0.5)
@@ -109,18 +107,31 @@ def main():
                     print("SWCVolDownBtn pushed @", message.timestamp)
                     time.sleep(0.5)
                     
-                elif message.arbitration_id == SWC and message.data[6]  in SWC_PHONE:
+                elif message.arbitration_id == SWC and message.data[6] in SWC_PHONE:
                     os.system("./rpotify.sh play")
                     os.system("./rpotify.sh status")
                     print("SWCPhoneBtn pushed @", message.timestamp)                    
                     time.sleep(0.5)
 
-                if message.arbitration_id == SWC and message.data[6] in RadioPhoneMode:
-                    os.system("./rpotify.sh stop")
-                    print("FM Radio or Phone Call Active @", message.timestamp)                    
-                    time.sleep(0.5)
-                 
-                    
+                elif message.arbitration_id == SWC and message.data[6] in AuxillaryMode:
+                    modeswap = 0
+                    if counter == 0:
+                        os.system("./rpotify.sh play")
+                        print("AuxillaryMode Active @", message.timestamp) 
+                        time.sleep(1)
+                        counter = counter + 1
+                    elif counter >= 1:            
+                        pass
+
+                elif message.arbitration_id == SWC and message.data[6] in RadioPhoneMode:
+                    counter = 0
+                    if modeswap == 0:
+                        os.system("./rpotify.sh stop")
+                        print("FM Radio or Phone Call Active @", message.timestamp) 
+                        time.sleep(1)
+                        modeswap = modeswap + 1
+                    elif modeswap == 1:
+                        pass                   
                 else:
                     pass        
                                                                            
@@ -143,3 +154,4 @@ if __name__ == "__main__":
     spotStart()
     spotDevices()
     
+k
